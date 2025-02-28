58c2c57f48e553179a3d0da19b0be9c3c21820de
mahmoudtarek1432 <43779204+mahmoudtarek1432@users.noreply.github.com>
2025-02-28 13:11:14 +0200

    Initiated kafka producer

--- Producer/Program.cs
+++ Producer/Program.cs
@@ -1,3 +1,5 @@
+using Confluent.Kafka;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -15,9 +17,34 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/weatherforecast", (ILogger<ProducerBuilder<string,string>> _logger, CancellationToken cancelationToken) =>
 {
-   
+    var config = new ProducerConfig
+    {
+        BootstrapServers = "localhost:9092",
+        AllowAutoCreateTopics = true,
+        Acks = Acks.All
+    };
+
+    using var producerBuilder = new ProducerBuilder<string,string>(config).Build();
+
+    try
+    {
+        var kafkaMessage = new Message<string, string>
+        {
+            Key = new Random().Next(999).ToString(),
+            Value = "Hello, Kafka"
+        };
+
+        var deliveryResult = producerBuilder.ProduceAsync("test-topic", kafkaMessage).Result;
+
+        _logger.LogInformation($"Message sent to Kafka {deliveryResult.TopicPartitionOffset}");
+    }
+    catch
+    {
+        _logger.LogWarning("Kafka provider error");
+    }
+
 })
 .WithName("GetWeatherForecast");
 
